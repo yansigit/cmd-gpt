f3a74da0cf5086c0d040db11aac0381b084512ec
Aiden Yoon <44089734+yansigit@users.noreply.github.com>
2025-01-05 13:34:44 -0700

    add multiline command support

--- handlers/command.go
+++ handlers/command.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
 
 	"github.com/charmbracelet/huh"
 
@@ -16,8 +19,7 @@ var logger *lib.Logger
 var cfg *lib.Config
 
 func HandleChat(commandType cnst.CommandType, shell string, input string, provider string) error {
-	var res string
-	systemPrompt := "You are a terminal assistant. Main purpose is to help the user to execute commands in the terminal. You will be given a command and you will execute it in the terminal. You will only output the command, no explanations."
+	systemPrompt := "You are a terminal assistant. Main purpose is to help the user to execute commands in the terminal. You will be given a command and you will execute it in the terminal. You will only output the full working, and valid commands without any explanations. No markdown output. Just plain commands."
 	prompt := input
 	if commandType == cnst.ShellCodeGen {
 		if shell == "" {
@@ -37,69 +39,145 @@ func HandleChat(commandType cnst.CommandType, shell string, input string, provid
 			}
 
 		}
-		prompt = fmt.Sprintf("Generate a %s command for: %s.", shell, input)
+		platform := runtime.GOOS
+		if platform == "darwin" {
+			platform = "macos"
+		}
+		prompt = fmt.Sprintf("I am on %s. Generate %s shell code for: %s.", runtime.GOOS, shell, input)
 	}
 
 	if provider == "" {
 		provider = cfg.DefaultProvider
 	}
 
-	var err error
-	switch provider {
-	case cnst.OpenAI:
-		res, err = HandleOpenAI(systemPrompt, prompt)
-		if err != nil {
-			return err
-		}
-	case cnst.Anthropic:
-		res, err = HandleAnthropic(systemPrompt, prompt)
-		if err != nil {
-			return err
-		}
-	case cnst.Google:
-		res, err = HandleGoogle(systemPrompt, prompt)
-		if err != nil {
-			return err
-		}
-	case cnst.OpenRouter:
-		res, err = HandleOpenRouter(systemPrompt, prompt)
-		if err != nil {
-			return err
-		}
-	default:
-		return logger.Errorf("invalid provider: %s", provider)
-	}
-
-	logger.Success("Command from GPT: ", res)
-	var confirmation bool
-	err = huh.NewConfirm().Title("Execute the command?").Value(&confirmation).Run()
+	res, err := generateCommand(provider, systemPrompt, prompt)
 	if err != nil {
-		logger.Errorf("error receiving confirmation: %v", err)
 		return err
 	}
-	if confirmation {
-		if err := executeCommand(res, shell); err != nil {
-			logger.Error("Error occurred during executing the command:", err)
-			return err
-		}
-	} else {
-		logger.Error("Skipped executing the command by the user.")
+
+	err = executeCommandWithConfirmation(res, shell, "Execute the command?", provider, systemPrompt, prompt)
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func executeCommand(command string, shell string) error {
-	var cmd *exec.Cmd
-	if shell == "powershell" {
-		cmd = exec.Command("powershell", "-Command", command)
-	} else {
-		cmd = exec.Command(shell, "-c", command)
+func generateCommand(provider string, systemPrompt string, prompt string) (string, error) {
+	var res string
+	var err error
+	switch provider {
+	case cnst.OpenAI:
+		res, err = HandleOpenAI(systemPrompt, prompt)
+	case cnst.Anthropic:
+		res, err = HandleAnthropic(systemPrompt, prompt)
+	case cnst.Google:
+		res, err = HandleGoogle(systemPrompt, prompt)
+	case cnst.OpenRouter:
+		res, err = HandleOpenRouter(systemPrompt, prompt)
+	default:
+		return "", fmt.Errorf("invalid provider: %s", provider)
 	}
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("failed to execute command: %v", err)
+	return trimMarkdownCodeTags(res), err
+}
+
+func executeCommandWithConfirmation(command string, shell string, message string, provider string, systemPrompt string, prompt string) error {
+	logger.Success("Command from GPT: ", command)
+	var confirmation bool
+	err := huh.NewConfirm().Title(message).Value(&confirmation).Run()
+	if err != nil {
+		logger.Errorf("error receiving confirmation: %v", err)
+		return err
+	}
+	if confirmation {
+		if err := executeCommand(command, shell); err != nil {
+			logger.Error("Error occurred during executing the command:", err)
+			return err
+		}
+	} else {
+		logger.Error("Skipped executing the command by the user.")
+		var regenerate bool
+		err = huh.NewConfirm().Title("Regenerate the command?").Value(&regenerate).Run()
+		if err != nil {
+			return fmt.Errorf("error receiving confirmation for regeneration: %v", err)
+		}
+		if regenerate {
+			var followUpPrompt string
+			huh.NewInput().
+				Title("Follow up prompt").
+				Prompt(">").
+				Value(&followUpPrompt).Run()
+			prompt = fmt.Sprintf("%s\n%s", prompt, followUpPrompt)
+			logger.Info("Regenerating the command...")
+			res, err := generateCommand(provider, systemPrompt, prompt)
+			if err != nil {
+				return err
+			}
+			err = executeCommandWithConfirmation(res, shell, "Execute the new command?", provider, systemPrompt, prompt)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func trimMarkdownCodeTags(input string) string {
+	openTagRegex := regexp.MustCompile("(?m)^```.*[ \n]")
+	result := openTagRegex.ReplaceAllString(input, "")
+
+	closeTagRegex := regexp.MustCompile("(?m)^```$")
+	result = closeTagRegex.ReplaceAllString(result, "")
+
+	return strings.TrimSpace(result)
+}
+
+func executeCommand(command string, shell string) error {
+	// Split command into lines to handle multiline commands
+	commands := strings.Split(command, "\n")
+
+	// If it's a multiline command, create a temporary shell file and execute that
+	if len(commands) > 1 {
+		f, err := os.CreateTemp("", "*."+strings.Split(shell, "/")[len(strings.Split(shell, "/"))-1])
+		if err != nil {
+			return fmt.Errorf("failed to create temporary file: %v", err)
+		}
+		defer os.Remove(f.Name()) // clean up
+
+		_, err = f.WriteString(command)
+		if err != nil {
+			return fmt.Errorf("failed to write to temporary file: %v", err)
+		}
+		if err := f.Close(); err != nil {
+			return fmt.Errorf("failed to close temporary file: %v", err)
+		}
+
+		cmd := exec.Command(shell, f.Name())
+		cmd.Dir = "." // Set working directory to current directory
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to execute command '%s': %v", command, err)
+		}
+	} else {
+		for _, cmdStr := range commands {
+			cmdStr = strings.TrimSpace(cmdStr)
+			if cmdStr == "" {
+				continue // Skip empty lines
+			}
+
+			var cmd *exec.Cmd
+			if shell == "powershell" {
+				cmd = exec.Command("powershell", "-Command", cmdStr)
+			} else {
+				cmd = exec.Command(shell, "-c", cmdStr)
+			}
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				return fmt.Errorf("failed to execute command '%s': %v", cmdStr, err)
+			}
+		}
 	}
 
 	return nil
